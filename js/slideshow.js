9ba3ad6781a0c6dd8eb8c10bdf04523bae0f5eb6
batu-1108 <93242382+batu-1108@users.noreply.github.com>
2022-03-23 11:21:44 +0900

    Add files via upload

---
+++ js/slideshow.js
@@ -0,0 +1,37 @@
+﻿
+//ハンバーガーメニュー
+$(function() {
+  $('.hamburger').click(function() {
+      $(this).toggleClass('active');
+
+      if ($(this).hasClass('active')) {
+          $('.globalMenuSp').addClass('active');
+      } else {
+          $('.globalMenuSp').removeClass('active');
+      }
+  });
+});
+
+//メインのスライド
+$(document).on('ready', function() {
+    $(".regular").slick({
+      autoplay: true,
+      autoplaySpeed: 4000,
+      dots: true,
+    });
+  });
+
+  jQuery(function ($) {
+    var fadeIn = $('.fade-in');
+    $(window).scroll(function () {
+      $(fadeIn).each(function () {
+        var offset = $(this).offset().top;
+        var scroll = $(window).scrollTop(); 
+        var windowHeight = $(window).height();
+        if (scroll > offset - windowHeight + 150) {
+          $(this).addClass("scroll-in");
+        }
+      });
+    });
+  });
+  
